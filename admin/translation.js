16b2b54613e5c173ee190d56415322b50624adb6
matheus simoes <matheusaugustosimoes@hotmail.com>
2020-12-19 11:36:02 -0300

    tradução de clientes

--- admin/translation.js
+++ admin/translation.js
@@ -14,7 +14,7 @@ module.exports = {
             filter: 'Filtro',
             applyChanges: 'Aplicar Mudanças',
             resetFilter: 'Resetar Mudanças',
-            confirmRemovalMany: 'Deseja realmente apagar permanentemente {{count}} registro?',
+            confirmRemovalMany: 'Deseja realmente apagar permanentemente {{count}} registros?',
             confirmRemovalMany_plural: 'Deseja realmente apagar permanentemente {{count}} registros?',
             logout: 'Sair',
             seeTheDocumentation: 'Veja: <1>na documentação</1>',
@@ -27,33 +27,67 @@ module.exports = {
             filters: 'Filtros',
             adminVersion: 'Admin: {{version}}',
             appVersion: 'App: {{version}}',
+            //Alterar nome das tabelas
+            Products: 'Produtos',
+            Providers: 'Fornecedores',
+            Clients: 'Clientes',
+            Inputs: 'Entrada de Produtos',
+            Clients: 'Clientes'
           },
           properties: {
         
           },
           resources: {
-        
+            Products:{
+                properties: {
+                    name: 'Nome do Produto',
+                    inStorage: 'Quantidade em estoque'
+                }
+            },
+            Inputs:{
+                //'id_product', 'quantity', 'value','id_provider'
+                properties:{
+                    id_product: "Nome do produto",
+                    quantity: "Quantidade (Caixas)",
+                    value: "Preço (Caixa)",
+                    id_provider: "Fornecedor"
+                }
+            },
+            Providers:{
+                //listProperties: ['name', 'phone', 'city','neighborhood','email']
+                properties:{
+                    name:'Nome',
+                    phone:'Telefone',
+                    city: 'Cidade',
+                    neighborhood:'Bairro',
+                    email: 'Email'
+                }
+            }
           },
+
+
+
+
           messages: {
-            successfullyBulkDeleted: ' {{count}} registro foi apagado com sucesso',
-            successfullyBulkDeleted_plural: ' {{count}} registros foram apagados com sucesso',
-            successfullyDeleted: 'Registro apagado com sucesso',
-            successfullyUpdated: 'Registro atualizado com sucesso',
-            thereWereValidationErrors: 'Existem erros de validação - por favor os confira para continuar',
-            successfullyCreated: 'Registro criado com sucesso',
-            bulkDeleteError: 'Ocorreu um erro ao deletar os registros, Confira o console para mais informações',
-            errorFetchingRecords: 'Ocorreu um erro ao inserir os registros, Confira o console para mais informações',
-            errorFetchingRecord: 'Ocorreu um erro ao inserir um registro, Confira o console para mais informações',
-            noRecordsSelected: 'Nenhum registro foi selecionado',
-            theseRecordsWillBeRemoved: 'Os seguintes registros serão apagados',
-            theseRecordsWillBeRemoved_plural: 'O seguinte registro será apagado',
-            pickSomeFirstToRemove: 'Para apagar algum registro, deve-se selecionar pelo menos um listado',
-            error404Resource: 'O registro de id: {{resourceId}} não pôde ser encontrado',
-            error404Action: 'O registro de id: {{resourceId}} não possui nenhum ação com o nome: {{actionName}}',
-            error404Record: 'O registro de id: {{resourceId}} não possui nenhum atributo com o id: {{recordId}}',
-            seeConsoleForMore: 'Confira o console de desenvolvimento para mais detalhes..',
-            noActionComponent: 'Você tem que implementar uma ação para a sua Ação',
-            noRecordsInResource: 'Não existem registros nessa coleção',
+            successfullyBulkDeleted: ' {{count}} registros foram apagados com sucesso.',
+            successfullyBulkDeleted_plural: ' {{count}} registros foram apagados com sucesso.',
+            successfullyDeleted: 'Registro apagado com sucesso.',
+            successfullyUpdated: 'Registro atualizado com sucesso.',
+            thereWereValidationErrors: 'Existem erros de validação - por favor os confira para continuar.',
+            successfullyCreated: 'Registro criado com sucesso.',
+            bulkDeleteError: 'Ocorreu um erro ao deletar os registros, Confira o console para mais informações.',
+            errorFetchingRecords: 'Ocorreu um erro ao inserir os registros, Confira o console para mais informações.',
+            errorFetchingRecord: 'Ocorreu um erro ao inserir um registro, Confira o console para mais informações.',
+            noRecordsSelected: 'Nenhum registro foi selecionado.',
+            theseRecordsWillBeRemoved: 'Os seguintes registros serão apagados.',
+            theseRecordsWillBeRemoved_plural: 'O seguinte registro será apagado.',
+            pickSomeFirstToRemove: 'Para apagar algum registro, deve-se selecionar pelo menos um listado.',
+            error404Resource: 'O registro de id: {{resourceId}} não pôde ser encontrado.',
+            error404Action: 'O registro de id: {{resourceId}} não possui nenhum ação com o nome: {{actionName}}.',
+            error404Record: 'O registro de id: {{resourceId}} não possui nenhum atributo com o id: {{recordId}}.',
+            seeConsoleForMore: 'Confira o console de desenvolvimento para mais detalhes...',
+            noActionComponent: 'Você tem que implementar uma ação para a sua Ação.',
+            noRecordsInResource: 'Não existem registros nessa coleção.',
             confirmDelete: 'Você realmente deseja deletar este registro?',
           },
     }
